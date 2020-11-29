8e4f2227b0b397a8646c27d051b0ee90636e6d9e
Applichic <applichic@gmail.com>
2020-11-29 22:39:33 +0100

    Adding new vault screen

---
+++ Chic Pass/Controllers/NewVaultController.swift
@@ -0,0 +1,21 @@
+//
+//  NewVaultController.swift
+//  Chic Pass
+//
+//  Created by Applichic on 11/29/20.
+//
+
+import UIKit
+
+class NewVaultController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onCancelClicked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
