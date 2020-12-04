a3033b8cdc7a72fe320aba24ff51e1381b786937
Applichic <applichic@gmail.com>
2020-12-04 23:18:10 +0100

    View or hide the password.

--- Chic Pass/Controllers/NewVaultController.swift
+++ Chic Pass/Controllers/NewVaultController.swift
@@ -17,6 +17,9 @@ class NewVaultController: UIViewController {
     @IBOutlet weak var goodView: UIView!
     @IBOutlet weak var veryGoodView: UIView!
     
+    private var isPasswordHidden = true
+    private var passwordIcon = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,10 +32,36 @@ class NewVaultController: UIViewController {
         nameTextField.addTarget(self, action: #selector(onTextFieldsChange(_:)), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(onTextFieldsChange(_:)), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(onPasswordStrengthChange(_:)), for: .editingChanged)
+        
+        // Add the password an eye icon
+        let passwordIconView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        passwordIcon = UIImageView(frame:CGRect(x: 50, y: 13, width: 24, height: 24))
+        passwordIcon.image = UIImage(systemName: "eye.fill")
+        passwordIcon.contentMode = .scaleAspectFit
+        passwordIcon.tintColor = UIColor.label
+        passwordIconView.addSubview(passwordIcon)
+        
+        let passwordIconTap = UITapGestureRecognizer(target: self, action: #selector(self.onPasswordIconTapped(_:)))
+        passwordIconView.addGestureRecognizer(passwordIconTap)
+        
+        passwordTextField.rightView = passwordIconView
+        passwordTextField.rightViewMode = .always
     }
 
     @IBAction func onCancelClicked(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func onPasswordIconTapped(_ sender: UITapGestureRecognizer? = nil) {
+        isPasswordHidden = !isPasswordHidden
+        
+        if isPasswordHidden {
+            passwordTextField.isSecureTextEntry = true
+            passwordIcon.image = UIImage(systemName: "eye.fill")
+        } else {
+            passwordTextField.isSecureTextEntry = false
+            passwordIcon.image = UIImage(systemName: "eye.slash.fill")
+        }
     }
     
     @objc func onTextFieldsChange(_ textField: UITextField) {
