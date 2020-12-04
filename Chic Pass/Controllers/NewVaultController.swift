574bd77a31efa479113629d45b047652b862c0b8
Applichic <applichic@gmail.com>
2020-12-04 22:47:39 +0100

    Add password strength

--- Chic Pass/Controllers/NewVaultController.swift
+++ Chic Pass/Controllers/NewVaultController.swift
@@ -12,6 +12,11 @@ class NewVaultController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var weakView: UIView!
+    @IBOutlet weak var mediumView: UIView!
+    @IBOutlet weak var goodView: UIView!
+    @IBOutlet weak var veryGoodView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +28,7 @@ class NewVaultController: UIViewController {
         
         nameTextField.addTarget(self, action: #selector(onTextFieldsChange(_:)), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(onTextFieldsChange(_:)), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(onPasswordStrengthChange(_:)), for: .editingChanged)
     }
 
     @IBAction func onCancelClicked(_ sender: Any) {
@@ -34,6 +40,35 @@ class NewVaultController: UIViewController {
             addButton.isEnabled = true
         } else {
             addButton.isEnabled = false
+        }
+    }
+    
+    @objc func onPasswordStrengthChange(_ textField: UITextField) {
+        if textField.text == nil || textField.text!.isEmpty {
+            weakView.backgroundColor = UIColor.secondarySystemBackground
+            mediumView.backgroundColor = UIColor.secondarySystemBackground
+            goodView.backgroundColor = UIColor.secondarySystemBackground
+            veryGoodView.backgroundColor = UIColor.secondarySystemBackground
+        } else if textField.text!.count <= 6 {
+            weakView.backgroundColor = UIColor.red
+            mediumView.backgroundColor = UIColor.secondarySystemBackground
+            goodView.backgroundColor = UIColor.secondarySystemBackground
+            veryGoodView.backgroundColor = UIColor.secondarySystemBackground
+        } else if textField.text!.count <= 12 {
+            weakView.backgroundColor = UIColor.orange
+            mediumView.backgroundColor = UIColor.orange
+            goodView.backgroundColor = UIColor.secondarySystemBackground
+            veryGoodView.backgroundColor = UIColor.secondarySystemBackground
+        }  else if textField.text!.count <= 16 {
+            weakView.backgroundColor = UIColor.green
+            mediumView.backgroundColor = UIColor.green
+            goodView.backgroundColor = UIColor.green
+            veryGoodView.backgroundColor = UIColor.secondarySystemBackground
+        } else {
+            weakView.backgroundColor = UIColor.green
+            mediumView.backgroundColor = UIColor.green
+            goodView.backgroundColor = UIColor.green
+            veryGoodView.backgroundColor = UIColor.green
         }
     }
 }
