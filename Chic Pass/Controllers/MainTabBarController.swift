336bf717d6d1c3a0508bd98bde6dedb7b5385c4f
Applichic <applichic@gmail.com>
2020-12-05 22:05:03 +0100

    Create a navigation controller for each tab

--- Chic Pass/Controllers/MainTabBarController.swift
+++ Chic Pass/Controllers/MainTabBarController.swift
@@ -8,21 +8,10 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-    @IBOutlet weak var addItem: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        if item.tag == 0 || item.tag == 1 {
-            addItem.isEnabled = true
-            addItem?.tintColor = UIColor.red
-        } else {
-            addItem.isEnabled = false
-            addItem?.tintColor = UIColor.clear
-        }
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 }
