0aefe5c2bb92c311375926111ab3f6473f29d7ff
applichic <applichic@gmail.com>
2020-12-23 18:02:39 +0100

    Adding settings and biometry screen.

--- Chic Pass/Controllers/SettingsController.swift
+++ Chic Pass/Controllers/SettingsController.swift
@@ -7,9 +7,27 @@
 
 import UIKit
 
-class SettingsController: UIViewController {
+class SettingsController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return 2
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 && indexPath.row == 1 {
+            performSegue(withIdentifier: "biometry", sender: nil)
+        }
     }
 }
